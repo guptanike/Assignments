f6772839cdd6b42c7231c9d9b907df6ab8d36112
agrawalchhavishankar-ai <agrawalchhavishankar@gmail.com>
2025-10-19 16:20:42 +0530

    first commit

--- Objective_2/mylib.c
+++ Objective_2/mylib.c
@@ -28,8 +28,12 @@ int isArmstrong(int num)
 }
 int reverseDigits(int num)
 {
-    int rev=0;
-    rev=rev*10+num%10;
+    int rev=0,temp=num;
+    while (temp!=0)
+    {
+        rev=rev*10+temp%10;
+        temp/=10;
+    }
     return rev;
 }
 int isAdams(int num)
